7141af373fd8f94466162d2c83a4128fe62b99b1
digda-5624 <hsg0208@naver.com>
2022-10-12 10:24:00 +0900

    [init] first initalize

---
+++ src/test/java/fisolution/jsonProject/JsonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package fisolution.jsonProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
